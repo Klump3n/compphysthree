afac5a5c60b591a1da994b77a1058a9e6da7616c
Matthias <plock@physik.hu-berlin.de>
2018-05-08 16:36:12 +0200

    Cosmetics

--- ue2/bin/aufg1/verify/conj_grad.py
+++ ue2/bin/aufg1/verify/conj_grad.py
@@ -3,36 +3,11 @@
 Implementation of the conjugate gradient method.
 
 """
-import time
 import numpy as np
 from laplace import laplace
 
-from cg import cg, two_norm
-
+from cg import cg
 from prototype import bare_vec
-
-def calc_residuum(R):
-    """
-    For every vector in R calculate the normalized residuum r.
-
-    """
-    return_r = []
-    for r in R:
-        return_r.append(two_norm(r))
-
-    return return_r
-
-def calc_errors(R, A):
-    """
-    Calculate the errors for every r in R.
-
-    """
-    Ainv = np.linalg.inv(A)
-    return_e = []
-    for r in R:
-        return_e.append(two_norm(np.dot(Ainv, r)))
-
-    return return_e
 
 
 if __name__ == '__main__':
