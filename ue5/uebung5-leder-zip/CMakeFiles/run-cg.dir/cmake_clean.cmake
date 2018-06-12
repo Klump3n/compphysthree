f433afd6f65787a935933588855e1e12983c79c7
PLedwon <paul.ledwon@protonmail.com>
2018-06-12 13:49:02 +0200

    old zip

---
+++ ue5/uebung5-leder-zip/CMakeFiles/run-cg.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/run-cg.dir/run-cg_generated_run-cg.cu.o"
+  "CMakeFiles/run-cg.dir/run-cg_generated_geometry.cu.o"
+  "CMakeFiles/run-cg.dir/run-cg_generated_linalg.cu.o"
+  "CMakeFiles/run-cg.dir/run-cg_generated_cg.cu.o"
+  "run-cg.pdb"
+  "run-cg"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/run-cg.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ue5/uebung5-leder-zip/cmake_install.cmake
@@ -0,0 +1,44 @@
+# Install script for directory: /users/stud/ledwon/compphysthree/ue5/uebung5-v1.0-714fe71f356d7f254fb5e2c2f55498d6364ad40b
+
+# Set the install prefix
+if(NOT DEFINED CMAKE_INSTALL_PREFIX)
+  set(CMAKE_INSTALL_PREFIX "/usr/local")
+endif()
+string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
+
+# Set the install configuration name.
+if(NOT DEFINED CMAKE_INSTALL_CONFIG_NAME)
+  if(BUILD_TYPE)
+    string(REGEX REPLACE "^[^A-Za-z0-9_]+" ""
+           CMAKE_INSTALL_CONFIG_NAME "${BUILD_TYPE}")
+  else()
+    set(CMAKE_INSTALL_CONFIG_NAME "")
+  endif()
+  message(STATUS "Install configuration: \"${CMAKE_INSTALL_CONFIG_NAME}\"")
+endif()
+
+# Set the component getting installed.
+if(NOT CMAKE_INSTALL_COMPONENT)
+  if(COMPONENT)
+    message(STATUS "Install component: \"${COMPONENT}\"")
+    set(CMAKE_INSTALL_COMPONENT "${COMPONENT}")
+  else()
+    set(CMAKE_INSTALL_COMPONENT)
+  endif()
+endif()
+
+# Install shared libraries without execute permission?
+if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
+  set(CMAKE_INSTALL_SO_NO_EXE "1")
+endif()
+
+if(CMAKE_INSTALL_COMPONENT)
+  set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
+else()
+  set(CMAKE_INSTALL_MANIFEST "install_manifest.txt")
+endif()
+
+string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+file(WRITE "/users/stud/ledwon/compphysthree/ue5/uebung5-v1.0-714fe71f356d7f254fb5e2c2f55498d6364ad40b/${CMAKE_INSTALL_MANIFEST}"
+     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
