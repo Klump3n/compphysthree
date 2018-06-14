05b11ad87b785bbe5eeb4029ddf86e2da910281f
PLedwon <paul.ledwon@protonmail.com>
2018-06-14 21:27:16 +0200

    tex more or less done, figure kommt morgen

--- ue5/scripts/speedupPlot.py
+++ ue5/scripts/speedupPlot.py
@@ -4,13 +4,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 Ndata = np.array([32,64,128,256,512,1024])
-avg_speedup = np.array([2,2,2,2,2,2])
+avg_speedup = np.array([0.0260783,0.0895114,0.3413917,0.851386,1.4301993,2.3417831])
 
 plt.figure()
 plt.plot(Ndata, avg_speedup)#, label=r'Speedup')
 #plt.title('Durchschnittlicher Speedup')
 plt.xlabel('Gittergroesse N')
-plt.ylabel('Speedup')
+plt.ylabel('Durchschnittlicher Speedup')
 #plt.xscale('log')
 #plt.legend(loc='best')
 plt.tight_layout()
