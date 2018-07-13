81103fae764dd574ece5fa1fea51f4339f827099
PLedwon <paul.ledwon@protonmail.com>
2018-07-13 10:59:14 +0200

    zipped up and ready 2 send

---
+++ ue9/bin/CMakeFiles/check.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/check.dir/check_generated_check.cu.o"
+  "CMakeFiles/check.dir/check_generated_geom_pbc.cu.o"
+  "CMakeFiles/check.dir/check_generated_spin.cu.o"
+  "CMakeFiles/check.dir/check_generated_metropolis.cu.o"
+  "CMakeFiles/check.dir/check_generated_randgpu.cu.o"
+  "CMakeFiles/check.dir/check_generated_added_stuff.cu.o"
+  "CMakeFiles/check.dir/check_generated_added_stuff_gpu.cu.o"
+  "check.pdb"
+  "check"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/check.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ue9/bin/CMakeFiles/run.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/run.dir/run_generated_run.cu.o"
+  "CMakeFiles/check.dir/check_generated_geom_pbc.cu.o"
+  "CMakeFiles/check.dir/check_generated_spin.cu.o"
+  "CMakeFiles/check.dir/check_generated_metropolis.cu.o"
+  "CMakeFiles/check.dir/check_generated_randgpu.cu.o"
+  "CMakeFiles/check.dir/check_generated_added_stuff.cu.o"
+  "CMakeFiles/check.dir/check_generated_added_stuff_gpu.cu.o"
+  "CMakeFiles/run.dir/run_generated_geom_pbc.cu.o"
+  "CMakeFiles/run.dir/run_generated_spin.cu.o"
+  "CMakeFiles/run.dir/run_generated_metropolis.cu.o"
+  "CMakeFiles/run.dir/run_generated_randgpu.cu.o"
+  "CMakeFiles/run.dir/run_generated_added_stuff.cu.o"
+  "CMakeFiles/run.dir/run_generated_added_stuff_gpu.cu.o"
+  "run.pdb"
+  "run"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/run.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
